b019454a3712733a5edc31e2dc7fe6a2ef41f05a
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-21 08:31:37 -0500

    Add navigator in app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import { StackNavigator } from './src/navigator/StackNavigator';
 const App = () => {
   return (
     <NavigationContainer>
-      <Text>App Screen</Text>
+      <StackNavigator/>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
