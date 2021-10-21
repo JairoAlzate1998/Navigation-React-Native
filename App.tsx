3699dd720fcebf49a680c1923723ac4f73976134
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-21 10:43:19 -0500

    add drawer navegation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import { StackNavigator } from './src/navigator/StackNavigator';
+//import { StackNavigator } from './src/navigator/StackNavigator';
+import { MenuLatertalBasico } from './src/navigator/MenuLateralBasico';
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      {/*<StackNavigator/>*/}
+      <MenuLatertalBasico/>
     </NavigationContainer>
   );
 };
---
+++ src/navigator/MenuLateralBasico.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {StackNavigator} from './StackNavigator';
+import { SettingScreen } from '../screens/SettingScreen';
+
+const Drawer = createDrawerNavigator();
+
+export const MenuLatertalBasico = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="SettingScreen" component={SettingScreen} />
+    </Drawer.Navigator>
+  );
+};
